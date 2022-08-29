3d2699072e6dbc151d15b7a3ce94b3610748fdab
glutenfre <94639100+glutenfre@users.noreply.github.com>
2022-08-29 17:54:09 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,25 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+
+#include <istream>
+#include <string>
+#include <vector>
+#include <string_view>
+
+namespace T_catalogue {
+
+namespace input {
+
+void Work_with(std::istream& input);
+
+void Read(const std::vector< std::string >& input_set);
+
+void ReadStops(TransportCatalogue& my_trans_cat, std::string_view s_v);
+
+void ReadStopsWithDim(TransportCatalogue& my_trans_cat, std::string_view s_v);
+
+void ReadBusses(TransportCatalogue& my_trans_cat, std::string_view s_v);
+
+}//namespace Insert
+}//namespace Transport_catalogue
