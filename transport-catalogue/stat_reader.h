54b72932dcfd1585732e2b108cde5e11995f1b3b
glutenfre <94639100+glutenfre@users.noreply.github.com>
2022-08-29 17:55:01 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,17 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+
+#include <string_view>
+
+namespace T_catalogue {
+
+namespace output {
+
+void F(TransportCatalogue& my_trans_cat, std::string_view s_v);
+
+void BusX(TransportCatalogue& my_trans_cat, std::string_view s_v);
+void StopX(TransportCatalogue& my_trans_cat, std::string_view s_v);
+
+}//namespace output
+}//namespace Transport_catalogue
