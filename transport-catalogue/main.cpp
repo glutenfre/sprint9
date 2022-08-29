61401d7d6969f05a4348303071971b6d3631556a
glutenfre <94639100+glutenfre@users.noreply.github.com>
2022-08-29 17:54:31 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,14 @@
-// место для вашего кода
+#include "input_reader.h"
+
+#include <string>
+#include <sstream>
+#include <iostream>
+
+
+int main() {
+
+    T_catalogue::input::Work_with(std::cin);
+
+    return 0;
+
+}
